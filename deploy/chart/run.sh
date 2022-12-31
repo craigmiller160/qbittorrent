e2b15e9da8f42f09cfe99d533296092424ea5582
Craig Miller <craigmiller160@gmail.com>
2022-12-31 17:23:47 -0500

    Progress

--- deploy/chart/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-run_script_dir=$(dirname "${BASH_SOURCE[0]}")
-source "$run_script_dir/../../scripts/helm_deploy.sh"
-
-arguments="--set environment=$1"
-OVERRIDE_NAMESPACE="apps-$1"
-
-run torrent $@
